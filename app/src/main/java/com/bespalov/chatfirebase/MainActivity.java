705e1fa526e478c0412cd0a9f3f71749c43f377b
Mikantare <abespalov33@gmail.com>
2021-10-17 14:29:40 +0300

    11

--- app/src/main/java/com/bespalov/chatfirebase/MainActivity.java
+++ app/src/main/java/com/bespalov/chatfirebase/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +20,7 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -36,10 +41,13 @@ public class MainActivity extends AppCompatActivity {
 
     private String userName;
 
+    private static final int RC_IMAGE_PICKER = 123;
+
     FirebaseDatabase database;
     DatabaseReference messagesDataBaseReference;
-
     ChildEventListener messagesChildEventListener;
+    DatabaseReference usersDataBaseReference;
+    ChildEventListener usersChildEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +55,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         database = FirebaseDatabase.getInstance();
         messagesDataBaseReference = database.getReference().child("messeges");
-        userName = "default name";
+        usersDataBaseReference = database.getReference().child("users");
 
+        Intent intent = getIntent();
+        if (intent != null) {
+            userName = intent.getStringExtra("userName");
+        } else {
+            userName = "default name";
+        }
         messageListView = findViewById(R.id.messageListView);
         progressBar = findViewById(R.id.progressBar);
         sendMessageButtont = findViewById(R.id.sendMessageButtont);
@@ -65,11 +79,11 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                    if (charSequence.toString().trim().length()>0) {
-                        sendMessageButtont.setEnabled(true);
-                    } else {
-                        sendMessageButtont.setEnabled(false);
-                    }
+                if (charSequence.toString().trim().length() > 0) {
+                    sendMessageButtont.setEnabled(true);
+                } else {
+                    sendMessageButtont.setEnabled(false);
+                }
             }
 
             @Override
@@ -78,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        messageEditText.setFilters(new InputFilter[] {new InputFilter.LengthFilter(500)});
+        messageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(500)});
 
         sendMessageButtont.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -96,9 +110,43 @@ public class MainActivity extends AppCompatActivity {
         sentImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intentToImage = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(intent,"choose an image"), RC_IMAGE_PICKER);
             }
         });
+        usersChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                User user = snapshot.getValue(User.class);
+                if (user.getId().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+                    userName = user.getName();
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        usersDataBaseReference.addChildEventListener(usersChildEventListener);
+
         messagesChildEventListener = new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
@@ -127,5 +175,24 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         messagesDataBaseReference.addChildEventListener(messagesChildEventListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sing_out:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this, SigninActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/bespalov/chatfirebase/SigninActivity.java
+++ app/src/main/java/com/bespalov/chatfirebase/SigninActivity.java
@@ -1,20 +1,156 @@
 package com.bespalov.chatfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SigninActivity extends AppCompatActivity {
 
+    private static final String TAG = "SigninActivity";
+
     private FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    DatabaseReference usersDataBaseReference;
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText repeatPasswordEditText;
+    private EditText nameEditText;
+    private Button signinButton;
+    private TextView toogleToLogIn;
+
+    private boolean logInModeActive;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
-        mAuth = FirebaseAuth.getInstance();
 
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        usersDataBaseReference = database.getReference().child("users");
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        repeatPasswordEditText = findViewById(R.id.repeatPasswordEditText);
+        nameEditText = findViewById(R.id.nameEditText);
+        signinButton = findViewById(R.id.signinButton);
+        toogleToLogIn = findViewById(R.id.toogleToLogIn);
+
+        signinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginSignUpUser(emailEditText.getText().toString().trim(), passwordEditText.getText().toString().trim());
+            }
+        });
+        if (mAuth.getCurrentUser() != null) {
+            startActivity(new Intent(SigninActivity.this, MainActivity.class));
+        }
+
+    }
+
+    private void loginSignUpUser(String email, String password) {
+        if (logInModeActive) {
+            if (passwordEditText.getText().toString().trim().length() < 6) {
+                Toast.makeText(this, "Password must be at least 6", Toast.LENGTH_SHORT).show();
+            } else if (emailEditText.getText().toString().trim().equals("")) {
+                Toast.makeText(this, "Please input your Email", Toast.LENGTH_SHORT).show();
+            } else {
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent intent = new Intent(SigninActivity.this, MainActivity.class);
+                                    intent.putExtra("userName", nameEditText.getText().toString().trim());
+                                    startActivity(intent);
+//                                updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(SigninActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+//                                updateUI(null);
+                                }
+                            }
+                        });
+
+            }
+        } else {
+            if (!passwordEditText.getText().toString().trim().equals(repeatPasswordEditText.getText().toString().trim())) {
+                Toast.makeText(this, "Confirm your password", Toast.LENGTH_SHORT).show();
+            } else if (passwordEditText.getText().toString().trim().length() < 6) {
+                Toast.makeText(this, "Password must be at least 6", Toast.LENGTH_SHORT).show();
+            } else if (emailEditText.getText().toString().trim().equals("")) {
+                Toast.makeText(this, "Please input your Email", Toast.LENGTH_SHORT).show();
+            } else {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    createUser(user);
+                                    Intent intent = new Intent(SigninActivity.this, MainActivity.class);
+                                    intent.putExtra("userName", nameEditText.getText().toString().trim());
+                                    startActivity(intent);
+//                            updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(SigninActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                                }
+                            }
+                        });
+            }
+        }
+
+
+    }
+
+    private void createUser(FirebaseUser firebaseUser) {
+        User user = new User();
+        user.setId(firebaseUser.getUid());
+        user.setEmail(firebaseUser.getEmail());
+        user.setName(nameEditText.getText().toString().trim());
+        usersDataBaseReference.push().setValue(user);
+    }
+
+    public void toggleLogInMode(View view) {
+        if (logInModeActive) {
+            logInModeActive = false;
+            signinButton.setText("Sign Up");
+            toogleToLogIn.setText("Or, log in");
+            repeatPasswordEditText.setVisibility(View.VISIBLE);
+        } else {
+            logInModeActive = true;
+            signinButton.setText("Log in");
+            toogleToLogIn.setText("Or, sign up");
+            repeatPasswordEditText.setVisibility(View.GONE);
+        }
     }
 }
